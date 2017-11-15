7335c5252fae1ee7baca7941dd8d79a4acaac147
Ilya Lapshin <podcherklife@gmail.com>
2017-11-15 22:03:54 +0300

    Removed useless joda module bean

--- chatbot/botframework-bot-impl/src/main/java/name/li/chatbot/ChatBotSpringBootApplication.java
+++ chatbot/botframework-bot-impl/src/main/java/name/li/chatbot/ChatBotSpringBootApplication.java
@@ -1,15 +1,8 @@
 package name.li.chatbot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.fasterxml.jackson.datatype.joda.JodaModule;
 
 @SpringBootApplication
 public class ChatBotSpringBootApplication {
 
-	@Bean
-	public JodaModule jodaModule() {
-		return new JodaModule();
-	}
 }
