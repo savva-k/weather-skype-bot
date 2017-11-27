6de36dd50fc4c04729893d99dd378eb788955de9
Savva Kodeikin <savva.kodeikin@gmail.com>
2017-11-27 22:50:05 +0300

    Added a class containing constants

---
+++ chatbot/botframework-bot-impl/src/main/java/name/li/chatbot/directline/ChatbotConstants.java
@@ -0,0 +1,11 @@
+package name.li.chatbot.directline;
+
+/**
+ * @author Savva Kodeikin
+ */
+public class ChatbotConstants {
+    public static final String HELP_MESSAGE = "The commands that the bot understands:\n\nhello\n\nhelp\n\n" +
+            "weather <location> <perion>, e.g. weather Ryazan today";
+
+    public static final String GREETINGS_MESSAGE = "Hi, this is Vasyatka bot. Type **help** for more information.";
+}
