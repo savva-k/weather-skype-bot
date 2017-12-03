867af29999b1a4f3cae4a82bd1dfe4dfaf9b3aee
Savva Kodeikin <savva.kodeikin@gmail.com>
2017-12-03 23:13:42 +0300

    Added logging for incoming activities

--- chatbot/botframework-bot-impl/src/main/java/name/li/chatbot/directline/Bot.java
+++ chatbot/botframework-bot-impl/src/main/java/name/li/chatbot/directline/Bot.java
@@ -8,9 +8,13 @@ import io.swagger.client.ApiException;
 import io.swagger.client.model.Activity;
 import name.li.chatbot.directline.dsl.ActivityMatcher;
 
+import java.util.logging.Logger;
+
 @RestController
 public class Bot {
-	
+
+	private static final Logger logger = Logger.getLogger(Bot.class.getName());
+
 	private ActivityMatcher activityMatcher;
 
 	public Bot(ActivityMatcher activityMatcher) {
@@ -19,6 +23,7 @@ public class Bot {
 
 	@RequestMapping("/")
 	public void echo(@RequestBody Activity in) throws ApiException {
+		logger.info("Incoming activity: " + in.toString());
 		activityMatcher.apply(in);
 	}
 
